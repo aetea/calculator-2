f7905d2710a8652b212cfe2a70e4cfe1ba7c0c68
Adele Teoh <vagrant@vagrant.vm>
2021-01-06 14:09:22 +0000

    Take user input, add numbers, exit calculator

--- calculator.py
+++ calculator.py
@@ -4,4 +4,43 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
-# Replace this with your code
+# repeat forever:
+while True:
+    # read input
+    print("Please enter an operator and two numbers")
+    user_input = input("> ")
+
+    # check if user asked to quit
+    if "q" in user_input:
+        print("Okay, you are exiting!")
+        break
+
+    # otherwise, keep going... #
+
+    # tokenise input
+    operator, num1, num2 = user_input.split(" ") 
+    num1 = int(num1)
+    num2 = int(num2)
+
+    # run various operators
+    if operator == "add": 
+        print (add(num1, num2))
+
+#         else:
+#             (decide which math function to call based on first token)
+#             if the first token is 'add':
+#                   call the power function with the other two tokens
+#             if the first token is 'subtract':
+#                   call the power function with the other two tokens
+#             if the first token is 'multiply':
+#                   call the power function with the other two tokens
+#             if the first token is 'divide':
+#                   call the power function with the other two tokens
+#             if the first token is 'cube':
+#                   call the power function with the other two tokens
+#             if the first token is 'power':
+#                   call the power function with the other two tokens
+#             if the first token is 'mod':
+#                   call the power function with the other two tokens
+#             if the first token is none of these:
+#                   return error message
